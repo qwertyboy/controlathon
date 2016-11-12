dcdf9b39bdd9dd50e038932e5a17d8a5cd8674a1
Haden Wasserbaech <spartanhaden@gmail.com>
2016-11-12 10:15:32 -0500

    Improved Formatting

--- src/BlinkLEDs.ino
+++ src/BlinkLEDs.ino
@@ -4,26 +4,24 @@ on the Zumo 32U4. */
 #include <Wire.h>
 #include <Zumo32U4.h>
 
-void setup()
-{
+void setup(){
 
 }
 
-void loop()
-{
-  // Turn the LEDs on.
-  ledRed(1);
-  ledYellow(1);
-  ledGreen(1);
+void loop(){
+    // Turn the LEDs on.
+    ledRed(1);
+    ledYellow(1);
+    ledGreen(1);
 
-  // Wait for a second.
-  delay(1000);
+    // Wait for a second.
+    delay(1000);
 
-  // Turn the LEDs off.
-  ledRed(0);
-  ledYellow(0);
-  ledGreen(0);
+    // Turn the LEDs off.
+    ledRed(0);
+    ledYellow(0);
+    ledGreen(0);
 
-  // Wait for a second.
-  delay(1000);
+    // Wait for a second.
+    delay(1000);
 }
