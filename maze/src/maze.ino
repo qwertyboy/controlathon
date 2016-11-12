46925a5b35f18b1040f237476d50f0f47f3719cb
Nathan Duprey <nathanduprey@hotmail.com>
2016-11-12 14:25:37 -0500

    turns

--- maze/src/maze.ino
+++ maze/src/maze.ino
@@ -17,9 +17,10 @@ bool proxLeftActive;
 bool proxFrontActive;
 bool proxRightActive;
 
-<<<<<<< HEAD
 int16_t gyroOffset;
 uint32_t gyroLastUpdate;
+uint32_t turnAngle = 0;
+int16_t turnRate;
 void gyroSetup(){
 	gyro.init();
 
@@ -61,6 +62,35 @@ void turnSensorReset(){
 	turnAngle = 0;
 }
 
+// Read the gyro and update the angle.  This should be called as
+// frequently as possible while using the gyro to do turns.
+void turnSensorUpdate()
+{
+	// Read the measurements from the gyro.
+	gyro.read();
+	turnRate = gyro.g.z - gyroOffset;
+
+	// Figure out how much time has passed since the last update (dt)
+	uint16_t m = micros();
+	uint16_t dt = m - gyroLastUpdate;
+	gyroLastUpdate = m;
+
+	// Multiply dt by turnRate in order to get an estimation of how
+	// much the robot has turned since the last update.
+	// (angular change = angular velocity * time)
+	int32_t d = (int32_t)turnRate * dt;
+
+	// The units of d are gyro digits times microseconds.  We need
+	// to convert those to the units of turnAngle, where 2^29 units
+	// represents 45 degrees.  The conversion from gyro digits to
+	// degrees per second (dps) is determined by the sensitivity of
+	// the gyro: 0.07 degrees per second per digit.
+	//
+	// (0.07 dps/digit) * (1/1000000 s/us) * (2^29/45 unit/degree)
+	// = 14680064/17578125 unit/(digit*us)
+	turnAngle += (int64_t)d * 14680064 / 17578125;
+}
+
 // Displays a bar graph of sensor readings on the LCD.
 // Returns after the user presses A.
 void showReadings()
@@ -80,8 +110,7 @@ void showReadings()
 	}
 }
 
-=======
->>>>>>> origin/master
+
 void setup()
 {
 	proxSensors.initThreeSensors();
@@ -137,18 +166,22 @@ void loop()
 	motors.setSpeeds(leftSpeed, rightSpeed);
 }
 
+int16_t turnSpeed = 200;
+const int32_t turnAngle45 = 0x20000000;
 void turnLeft(){
 	// determines if the robot turns left or right.
-	int16_t leftSpeed = constrain(leftSpeed, 0, (int16_t)maxSpeed);
-	int16_t rightSpeed = constrain(rightSpeed, 0, (int16_t)maxSpeed);
-
-	motors.setSpeeds(leftSpeed, rightSpeed);
+	motors.setSpeeds(-turnSpeed, turnSpeed);
+	while((int32_t)turnAngle < turnAngle45 * 2){
+		turnSensorUpdate();
+	}
+	motors.setSpeeds(0, 0);
 }
 
 void turnRight(){
 	// determines if the robot turns left or right.
-	int16_t leftSpeed = constrain(leftSpeed, 0, (int16_t)maxSpeed);
-	int16_t rightSpeed = constrain(rightSpeed, 0, (int16_t)maxSpeed);
-
-	motors.setSpeeds(leftSpeed, rightSpeed);
+	motors.setSpeeds(turnSpeed, -turnSpeed);
+	while((int32_t)turnAngle > turnAngle45 * -2){
+		turnSensorUpdate();
+	}
+	motors.setSpeeds(0, 0);
 }
