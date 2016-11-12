b6776670a1898e9c1af70242185375e9bdb076e4
Haden Wasserbaech <spartanhaden@gmail.com>
2016-11-12 12:02:47 -0500

    Cleaned up the code

--- src/BlinkLEDs.ino
+++ src/BlinkLEDs.ino
@@ -1,11 +1,3 @@
-/** This example shows how to read from the five line sensors and
-three proximity sensors on the Zumo32U4 Front Sensor Array and
-documents the different options available for configuring those
-sensors.
-
-The sensor readings are displayed on the LCD and also printed to
-the serial monitor. */
-
 #include <Wire.h>
 #include <Zumo32U4.h>
 
@@ -14,93 +6,36 @@ Zumo32U4LineSensors lineSensors;
 Zumo32U4ProximitySensors proxSensors;
 Zumo32U4Motors motors;
 
-
 uint16_t lineSensorValues[5] = { 0, 0, 0, 0, 0 };
 
 bool proxLeftActive;
 bool proxFrontActive;
 bool proxRightActive;
 
-void setup()
-{
-  lineSensors.initThreeSensors();
-  proxSensors.initThreeSensors();
-
-  loadCustomCharacters();
+void setup() {
+	lineSensors.initThreeSensors();
+	proxSensors.initThreeSensors();
 }
 
-void loadCustomCharacters()
-{
-  static const char levels[] PROGMEM = {
-    0, 0, 0, 0, 0, 0, 0, 63, 63, 63, 63, 63, 63, 63
-  };
-  lcd.loadCustomCharacter(levels + 0, 0);  // 1 bar
-  lcd.loadCustomCharacter(levels + 1, 1);  // 2 bars
-  lcd.loadCustomCharacter(levels + 2, 2);  // 3 bars
-  lcd.loadCustomCharacter(levels + 3, 3);  // 4 bars
-  lcd.loadCustomCharacter(levels + 4, 4);  // 5 bars
-  lcd.loadCustomCharacter(levels + 5, 5);  // 6 bars
-  lcd.loadCustomCharacter(levels + 6, 6);  // 7 bars
-}
+void loop() {
+	static uint16_t lastSampleTime = 0;
 
-void printBar(uint8_t height)
-{
-  if (height > 8) { height = 8; }
-  const char barChars[] = {' ', 0, 1, 2, 3, 4, 5, 6, 255};
-  lcd.print(barChars[height]);
-}
+	if ((uint16_t)(millis() - lastSampleTime) >= 100) {
+		lastSampleTime = millis();
 
-// Prints a line with all the sensor readings to the serial
-// monitor.
-void printReadingsToSerial()
-{
-  static char buffer[80];
-  sprintf(buffer, "%d %d %d %d %d %d  %d %d %d  %4d %4d %4d %4d %4d\n",
-    proxSensors.countsLeftWithLeftLeds(),
-    proxSensors.countsLeftWithRightLeds(),
-    proxSensors.countsFrontWithLeftLeds(),
-    proxSensors.countsFrontWithRightLeds(),
-    proxSensors.countsRightWithLeftLeds(),
-    proxSensors.countsRightWithRightLeds(),
-    proxLeftActive,
-    proxFrontActive,
-    proxRightActive,
-    lineSensorValues[0],
-    lineSensorValues[1],
-    lineSensorValues[2],
-    lineSensorValues[3],
-    lineSensorValues[4]
-  );
-  Serial.print(buffer);
-}
+		// Send IR pulses and read the proximity sensors.
+		proxSensors.read();
 
-void loop()
-{
-  static uint16_t lastSampleTime = 0;
+		// Just read the proximity sensors without sending pulses.
+		proxLeftActive = proxSensors.readBasicLeft();
+		proxFrontActive = proxSensors.readBasicFront();
+		proxRightActive = proxSensors.readBasicRight();
 
-  if ((uint16_t)(millis() - lastSampleTime) >= 100)
-  {
-    lastSampleTime = millis();
-
-    // Send IR pulses and read the proximity sensors.
-    proxSensors.read();
-
-    // Just read the proximity sensors without sending pulses.
-    proxLeftActive = proxSensors.readBasicLeft();
-    proxFrontActive = proxSensors.readBasicFront();
-    proxRightActive = proxSensors.readBasicRight();
-
-    // Read the line sensors.
-    //lineSensors.readCalibrated(lineSensorValues);
-
-    // Send the results to the LCD and to the serial monitor.
-    //printReadingsToLCD();
-    printReadingsToSerial();
 	drive();
-  }
+	}
 }
 
-void drive(){
+void drive() {
 	int left = proxSensors.countsLeftWithLeftLeds();
 	int right = proxSensors.countsRightWithRightLeds();
 
